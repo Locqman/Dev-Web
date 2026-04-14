5524262eec7a08cfa07371c87f5247254dbd8155
Locqman <benyoucefloko@gmail.com>
2026-04-14 23:30:45 +0200

    Set up MySQL connection in db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,18 @@
+const mysql = require('mysql2');
 
+const db = mysql.createConnection({
+  host:     'localhost',
+  user:     'root',        // à changer selon votre config MySQL
+  password: '',            // à changer selon votre config MySQL
+  database: 'skiconnect'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Erreur connexion MySQL :', err.message);
+    process.exit(1);
+  }
+  console.log('Connecté à MySQL ✅');
+});
+
+module.exports = db;
